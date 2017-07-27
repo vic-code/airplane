2742de1f02dfe4f13ade7fa655264c8e05ba0e0c
vic <chinese_99999@163.com>
2017-07-27 22:45:41 +0800

    游戏开始，加入了我方飞机，跟随鼠标位置动

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  * @Author: lenovo
  * @Date:   2017-07-20 21:33:28
  * @Last Modified by:   lenovo
- * @Last Modified time: 2017-07-23 18:21:58
+ * @Last Modified time: 2017-07-27 22:44:57
  */
 
 'use strict';
@@ -111,66 +111,203 @@ logo.src = "images/start.png";
 
 //游戏开始
 canvas.onclick = function() {
-	if(state==START){
-		state=STARTTING;
-	}else if(state == RUNNING){
-		hero.switchBullet = !hero.switchBullet; //子弹切换
-	}
+    if (state == START) {
+        state = STARTTING;
+    } else if (state == RUNNING) {
+        hero.switchBullet = !hero.switchBullet; //子弹切换
+    }
 }
 
-var loadings=[];
-loadings[0]=new Image();
-loadings[0].src="images/game_loading1.png";
-loadings[1]=new Image();
-loadings[1].src="images/game_loading2.png";
-loadings[2]=new Image();
-loadings[2].src="images/game_loading3.png";
-loadings[3]=new Image();
-loadings[3].src="images/game_loading4.png";
+var loadings = [];
+loadings[0] = new Image();
+loadings[0].src = "images/game_loading1.png";
+loadings[1] = new Image();
+loadings[1].src = "images/game_loading2.png";
+loadings[2] = new Image();
+loadings[2].src = "images/game_loading3.png";
+loadings[3] = new Image();
+loadings[3].src = "images/game_loading4.png";
 
-var LOADING={
-	imgs:loadings,
-	width:186,
-	height:38,
-	sum:loadings.length
+var LOADING = {
+    imgs: loadings,
+    width: 186,
+    height: 38,
+    sum: loadings.length
 }
-function Loading(config){  //加载动画构造器
-	this.imgs=config.imgs;
-	this.width=config.width;
-	this.height=config.height;
-	this.sum=config.sum;
-	//索引
-	this.frameIndex=0;
 
-	this.paint=function(cxt){
-		cxt.drawImage(this.imgs[this.frameIndex], 0, HEIGHT - this.height);
-	}
+function Loading(config) { //加载动画构造器
+    this.imgs = config.imgs;
+    this.width = config.width;
+    this.height = config.height;
+    this.sum = config.sum;
+    //索引
+    this.frameIndex = 0;
 
-	this.speed=0;
-	this.step=function(){
-		this.speed++;
-		if(this.speed%3==0){
-			this.frameIndex++;
-		}
-		if(this.frameIndex==4){
-			state = RUNNING;
-		}
-	} 
+    this.paint = function(cxt) {
+        cxt.drawImage(this.imgs[this.frameIndex], 0, HEIGHT - this.height);
+    }
+
+    this.speed = 0;
+    this.step = function() {
+        this.speed++;
+        if (this.speed % 3 == 0) {
+            this.frameIndex++;
+        }
+        if (this.frameIndex == 4) {
+            state = RUNNING;
+        }
+    }
 }
-var loading=new Loading(LOADING);
+var loading = new Loading(LOADING);
+
+
+
+//我方飞机
+var heros = [];
+heros[0] = new Image();
+heros[0].src = "images/hero1.png";
+heros[1] = new Image();
+heros[1].src = "images/hero2.png";
+heros[2] = new Image();
+heros[2].src = "images/hero_blowup_n1.png";
+heros[3] = new Image();
+heros[3].src = "images/hero_blowup_n2.png";
+heros[4] = new Image();
+heros[4].src = "images/hero_blowup_n3.png";
+heros[5] = new Image();
+heros[5].src = "images/hero_blowup_n4.png";
+
+var HERO = {
+    imgs: heros,
+    width: 99,
+    height: 124,
+    sum: heros.length
+}
+//英雄飞机
+function Hero(config) {
+    this.imgs = config.imgs;
+    this.width = config.width;
+    this.height = config.height;
+    this.sum = config.sum;
+
+    this.frameIndex = 0;
+
+    this.x = (WIDTH - this.width) / 2;
+    this.y = HEIGHT - this.height - 30;
+
+    //是否爆破
+    this.broken = false;
+
+    this.switchBullet = false; //切换子弹
+
+    this.paint = function(cxt) {
+        cxt.drawImage(this.imgs[this.frameIndex], this.x, this.y);
+    }
+
+    this.step = function() {
+        if (this.broken) {
+            this.frameIndex++;
+            if (this.frameIndex == this.sum) {
+                this.frameIndex = this.sun - 1;
+                life--;
+                if (life == 0) {
+                    state = GAMEOVER;
+                } else {
+                    hero = new Hero(HERO);
+                }
+            }
+        } else {
+            this.frameIndex = (++this.frameIndex) % 2;
+        }
+    }
+
+    //射击
+    this.shootSpeed = 0;
+    this.shoot=function(){
+        this.shootSpeed++;
+        if(this.shootSpeed%3==0){
+             if (!this.switchBullet) {
+              bullets[bullets.length] = new Bullet(BULLET);
+            } else {
+              san_bullets[san_bullets.length] = new Bullet(BULLET);
+            }
+        }
+    }
+
+    //撞击后的方法
+    this.brokenDown=function(){
+        this.broken=true;
+        this.frameIndex = 2;  //爆破动画
+    }
+}
+
+var hero = new Hero(HERO);
+
+canvas.onmousemove = function (event) {
+    if (state == RUNNING) {
+      // a. 鼠标坐标值:pageX|clientX|offsetX|x
+      // b. 根据鼠标当前坐标值,修改hero的坐标值
+      hero.x = event.offsetX - hero.width / 2;
+      hero.y = event.offsetY - hero.height / 2;
+    }
+  }
+
+//手机触摸屏
+if(!PC){
+    (function(){
+    var hasTouch = true;
+      var touchStart = hasTouch ? 'touchstart' : 'mousedown';
+      var touchMove = hasTouch ? 'touchmove' : 'mousemove';
+      var touchEnd = hasTouch ? 'touchend' : 'mouseup';
+
+      var start = function (e) {
+        var point = hasTouch ? e.touches[0] : e;
+        //添加“触摸移动”事件监听
+        canvas.addEventListener(touchMove, move, false);
+        //添加“触摸结束”事件监听
+        canvas.addEventListener(touchEnd, end, false);
+      }
+
+      var move = function (e) {
+        var point = hasTouch ? e.touches[0] : e;
+        e.preventDefault();
+        if (state == RUNNING) {
+          hero.x = point.pageX - hero.width / 2;
+          hero.y = point.pageY - hero.height / 2;
+        }
+      }
+
+      var end = function (e) {
+        canvas.removeEventListener(touchStart, end, false);
+        canvas.removeEventListener(touchMove, move, false);
+        canvas.removeEventListener(touchEnd, end, false);
+      }
+      //添加“触摸开始”事件监听
+      canvas.addEventListener(touchStart, start, false);
+    })();
+}
+
+//子弹类
+
+
 
 
 //游戏控制器
-setInterval(function(){
-	backg.paint(context);  //背景
-	backg.step();  //动画
-	switch(state){
+setInterval(function() {
+    backg.paint(context); //背景
+    backg.step(); //动画
+    switch (state) {
         case START:
-            context.drawImage(logo, 0, 0, WIDTH, HEIGHT);//logo
+            context.drawImage(logo, 0, 0, WIDTH, HEIGHT); //logo
             break;
         case STARTTING:
             loading.paint(context);
             loading.step();
-        break;
+            break;
+        case RUNNING:
+            hero.paint(context);//绘制方法
+            hero.step();//动画方法
+            //hero.shoot();//射击方法
+            break;
     }
-},60);
+}, 60);
